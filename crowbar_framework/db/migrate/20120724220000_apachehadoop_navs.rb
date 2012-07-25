f736436e683c3d20f1b519f5032f5cda559a03ef
Greg Althaus <galthaus@austin.rr.com>
2012-07-25 12:30:35 -0500

    Move out nav and locales

--- crowbar_framework/db/migrate/20120724220000_apachehadoop_navs.rb
+++ crowbar_framework/db/migrate/20120724220000_apachehadoop_navs.rb
@@ -1,10 +1,10 @@
-# Copyright 2012, Dell, Inc.
+# Copyright 2012, Dell
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,20 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Author: RobHirschfeld
 #
+class ApachehaddopNavs < ActiveRecord::Migration
+  def self.up
+    
+    Nav.find_or_create_by_item :item=>'apachehadoop', :parent_item=>'barclamps', :name=>'nav.apachehadoop', :description=>'nav.apachehadoop_description', :path=>"index_barclamp_path(:controller=>'apachehadoop')", :order=>500
 
-barclamp:
-  name: apachehadoop
-  display: Apache Hadoop framework barclamp collection
-  version: 0
-  user_managed: false
-  requires:
-    - @crowbar
-  member:
-    - apachehadoop
+  end
 
-crowbar:
-  layout: 1
-  order: 200
-
+  def self.down
+    Nav.delete_by_item 'apachehadoop'
+  end
+end
